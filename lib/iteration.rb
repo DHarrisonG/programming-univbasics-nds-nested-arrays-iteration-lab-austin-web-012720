35bf3d49ac74d7395b12b115f61b8cf257b3fc42
Doug Harrison <dougharrisong@gmail.com>
2020-01-10 18:02:17 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,6 +42,7 @@ def total_even_pairs(src)
   total = 0
   while i < src.length do 
     if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
-      total = 
+      total += (src[i][0] + src[i][1])
+      i +=1
 end
 
